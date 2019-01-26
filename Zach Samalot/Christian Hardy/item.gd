9c49908772bc22cfcc9c0c5eec6d89718c606300
Zachtoplasm <45503867+Zachtoplasm@users.noreply.github.com>
2019-01-26 12:07:01 -0500

    copied projects from Zach, Chris, Raea and Tony

---
+++ Zach Samalot/Christian Hardy/item.gd
@@ -0,0 +1,84 @@
+extends Area2D
+
+
+"""
+
+	Christian Hardy
+	1/25/2018
+	working on 
+
+TODO:
+	signals
+	
+"""
+
+
+#signal PickUp
+
+
+export(Texture) var ItemImage
+export(String) var ItemName
+var player
+onready var toggle = $Button.visible
+
+
+func Output():
+	return ItemName
+	
+
+func PickUp():
+	
+	queue_free()
+#	emit_signal("PickUp")
+	pass
+
+func TextureLoad(image):
+	$Sprite.texture = image
+
+
+func _ready():
+	TextureLoad(ItemImage)
+
+func ButtonToggle():
+	if toggle:
+		toggle = false
+		$Button.visible = toggle
+	else:
+		toggle = true
+		$Button.visible = toggle
+
+func _on_Items_area_entered(area):
+	if area.is_in_group('player'):
+		print('in')
+		player = area
+		ButtonToggle()
+		
+
+
+func _on_Button_pressed():
+	PickUp()
+
+func _on_Items_area_exited(area):
+	if area.is_in_group("player"):
+		ButtonToggle()
+
+
+func _on_Items_body_entered(body):
+	if body.is_in_group('player'):
+		print('in')
+		player = body
+		ButtonToggle()
+
+
+
+func _on_Items_body_exited(body):
+	if body.is_in_group("player"):
+		ButtonToggle()
+		
+
+
+func _process(delta):
+	if toggle:
+		if Input.is_action_pressed("ui_accept"):
+			PickUp()
+	
