fcd433a218ad8f963902a7ba4471ec9cb64b0e84
SUNY-GDC <45503453+SUNY-GDC@users.noreply.github.com>
2019-01-26 13:42:11 -0500

    Saturation Detection Updated

--- Raea Adams/Scripts/Player.gd
+++ Raea Adams/Scripts/Player.gd
@@ -41,6 +41,7 @@ func _process(delta):
 	velocity = Vector2(0, 0)
 	get_input()
 	move_and_slide(velocity) * speed
+	saturation_update() #Updates the saturation level
 
 func get_input():
 	#Variable input shortcuts
@@ -62,6 +63,60 @@ func get_input():
 # SANITY RESET ON ITEM PICKUP
 # whenever the signal is received that an item has been picked up
 # call reset_sanity()
+
+#I did math!
+func saturation_update():
+	var distance #Distance is how far away is an object
+	var for_count = 0 #This is use for the for statement to count the objects
+	
+	#For each object in the objects array
+	for current in objects: 
+		distance = objects[for_count].Pos.distance_to(self.position) #Distance is set to the current object's distance from the mouse or the character
+		
+		#If it's the first object
+		if for_count == 0: 
+			closest_pos = distance #The closest position is set to the first object by default
+			closest_obj = objects[for_count].Name #The closest object is set to the first object by default
+		
+		#If the distance is less than the closest position so far
+		if distance < closest_pos: 
+			closest_obj = objects[for_count].Name #Sets a new closest object
+			closest_pos = distance #Sets a new closest position
+		for_count += 1 #Adds for count by 1
+		
+		#This handles range of when the different saturation levels occur
+		if closest_pos > 3000: #Main Change
+			$CharCam/Saturation.environment.adjustment_saturation = 0.01
+		if closest_pos == 3000: #Main Change
+			$CharCam/Saturation.environment.adjustment_saturation = 0.03
+		if closest_pos < 3000 and closest_pos >= 2700:
+			$CharCam/Saturation.environment.adjustment_saturation = 0.05
+		if closest_pos < 2700 and closest_pos >= 2600:
+			$CharCam/Saturation.environment.adjustment_saturation = 0.07
+		if closest_pos < 3000 and closest_pos >= 2500: #Main Change
+			$CharCam/Saturation.environment.adjustment_saturation = 0.2
+		if closest_pos < 2500 and closest_pos >= 2300:
+			$CharCam/Saturation.environment.adjustment_saturation = 0.22
+		if closest_pos < 2300 and closest_pos >= 2100:
+			$CharCam/Saturation.environment.adjustment_saturation = 0.25
+		if closest_pos < 2500 and closest_pos >= 2000: #Main Change
+			$CharCam/Saturation.environment.adjustment_saturation = 0.3
+		if closest_pos < 2000 and closest_pos >= 1500: #Main Change
+			$CharCam/Saturation.environment.adjustment_saturation = 0.4
+		if closest_pos < 1500 and closest_pos >= 1000: #Main Change
+			$CharCam/Saturation.environment.adjustment_saturation = 0.5
+		if closest_pos < 1000 and closest_pos >= 500: #Main Change
+			$CharCam/Saturation.environment.adjustment_saturation = 0.6
+		if closest_pos < 500 and closest_pos > 350: #Main Change
+			$CharCam/Saturation.environment.adjustment_saturation = 0.7
+		if closest_pos < 200: #Main Change
+			$CharCam/Saturation.environment.adjustment_saturation = 1.0
+
+
+#Couldn't find the proper property path
+#func tween_it(new_sat):
+#	$Tween.interpolate_property($CharCam/Saturation, "CharCam/Saturation:Environment:Adjustments:Saturation", $CharCam/Saturation.environment.adjustment_saturation, new_sat, 0.3,Tween.TRANS_SINE,Tween.EASE_IN_OUT)
+#	$Tween.start()
 
 func _on_SanityTimer_timeout():
 	new_game_plus.Sanity -= new_game_plus.SanityDecrease # decrease your sanity on timeout
